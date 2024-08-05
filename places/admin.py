c4bc3431b2069b3b86ff11b4dc5070256bbeeb97
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-05 16:40:05 +0300

    add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location, Image
 
 
 # Register your models here.
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image')
--- places/models.py
+++ places/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
+class Image(models.Model):
+    title = models.CharField(max_length=255, default='')
+    image = models.ImageField(upload_to='places/images')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
 class Location(models.Model):
     title = models.CharField(max_length=100, default='')
+    image = models.ManyToManyField(Image, null=True, blank=True, related_name='images')
     description_short = models.TextField(null=True, blank=True)
     description_long = models.TextField(null=True, blank=True)
     lng = models.DecimalField(max_digits=17, decimal_places=14, default=37.6155600)
@@ -10,3 +19,6 @@ class Location(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+
+
