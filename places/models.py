0c35ae545c181262721fd73bedc60064f3cb13a3
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-07 12:29:09 +0300

    regroup fields for image FK of Location model

--- places/models.py
+++ places/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 from enjoymap.settings import MEDIA_URL
 
-class Image(models.Model):
-    title = models.CharField(max_length=255, default='')
-    filename = models.ImageField(upload_to='places/images')
-
-    def __str__(self):
-        return f'{self.title}'
-
 
 class Location(models.Model):
-    title = models.CharField(max_length=100, default='')
+    title = models.CharField(max_length=100, default='', verbose_name='Название')
     place_id = models.CharField(max_length=100, default='')
     detailsUrl = models.CharField(max_length=100, default='')
-    image = models.ManyToManyField(Image, related_name='locations')
-    description_short = models.TextField(null=True, blank=True)
-    description_long = models.TextField(null=True, blank=True)
-    lng = models.DecimalField(max_digits=18, decimal_places=14, default=37.6155600)
-    lat = models.DecimalField(max_digits=18, decimal_places=14, default=55.7522200)
+    description_short = models.TextField(null=True, blank=True, verbose_name='Краткое описание')
+    description_long = models.TextField(null=True, blank=True, verbose_name='Подробное описание')
+    lng = models.DecimalField(max_digits=18, decimal_places=14, default=37.6155600, verbose_name='Долгота')
+    lat = models.DecimalField(max_digits=18, decimal_places=14, default=55.7522200, verbose_name='Широта')
 
     def __str__(self):
         return f'{self.title}'
+
+
+class Image(models.Model):
+    filename = models.ImageField(upload_to='places/images', verbose_name='Картинка')
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, related_name='images', null=True)
+    position = models.IntegerField(verbose_name='Позиция', null=True)
+
+    def __str__(self):
+        return f'{self.filename}'
