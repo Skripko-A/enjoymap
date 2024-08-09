5e11a75cc8ed7e9042cdcb31875c8f5216f7d65d
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-09 15:10:52 +0300

    only local use

--- enjoymap/urls.py
+++ enjoymap/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.staticfiles.views import serve
+from django.urls import path, include, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,3 +26,4 @@ urlpatterns = [
                   path('places/<int:location_id>', views.show_location, name='location_page'),
                   path('tinymce/', include('tinymce.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
