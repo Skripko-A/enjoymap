3a54685ae1b2fa69a5afefdc86cb17e83dbede14
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-27 10:19:49 +0300

    sort imports

--- enjoymap/urls.py
+++ enjoymap/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
