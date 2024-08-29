61436274bcedef1900b2aa83f3f39ef3c9cad0a9
Aleksandr Skripko <skripkoae@gmail.com>
2024-08-29 07:41:33 +0300

    correct variable names

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -5,29 +5,29 @@ from django.core.management.base import BaseCommand
 from places.models import Location, Image
 
 
-def get_location_json(json_url):
+def get_location_params(json_url):
     response = requests.get(json_url)
     response.raise_for_status()
     return response.json()
 
 
-def post_location(location_json):
-    location, created = Location.objects.get_or_create(title=location_json['title'],
+def post_location(location_params):
+    location, created = Location.objects.get_or_create(title=location_params['title'],
         defaults={
-            'short_description': location_json['description_short'],
-            'long_description': location_json['description_long'],
-            'lng': location_json['coordinates']['lng'],
-            'lat': location_json['coordinates']['lat']
+            'short_description': location_params['description_short'],
+            'long_description': location_params['description_long'],
+            'lng': location_params['coordinates']['lng'],
+            'lat': location_params['coordinates']['lat']
         })
     return location.id
 
 
-def fetch_and_post_image(image_url, location_id, image_number, location_json):
+def fetch_and_post_image(image_url, location_id, image_number, location_params):
     response = requests.get(image_url)
     response.raise_for_status()
 
     file_format = image_url.rsplit('.', 1)[-1].lower()
-    filename = f'{location_json["title"]}_{image_number}.{file_format}'
+    filename = f'{location_params["title"]}_{image_number}.{file_format}'
     content_file = ContentFile(response.content, name=filename)
     Image.objects.get_or_create(location_id=location_id, position=image_number, file=content_file)
 
@@ -41,7 +41,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for url in options['json_urls']:
             try:
-                serialize_location = get_location_json(url)
+                serialize_location = get_location_params(url)
                 location_id = post_location(serialize_location)
                 for image_number, image_url in enumerate(serialize_location['imgs'], start=1):
                     fetch_and_post_image(image_url, location_id, image_number, serialize_location)
